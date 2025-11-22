3af64895942c2266b9889c7086adde51c1fbf78f
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2025-11-21 22:44:46 -0700

    Major Issues fixed before Meet 1 - Added telemetry to drivetrainTest.java. **DISABLE THIS OPMODE AFTER TESTING IS COMPLETE**

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomi/drivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomi/drivetrainTest.java
@@ -17,12 +17,10 @@ public class drivetrainTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        DcMotor intake = hardwareMap.dcMotor.get("intake");
-        DcMotor shooter = hardwareMap.dcMotor.get("shooter");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor"); //CH Motor Port 0
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor"); //CH Motor Port 1
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor"); //CH Motor Port 2
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor"); //CH Motor Port 3
 
         movement = new Movement(hardwareMap);
 
@@ -30,18 +28,37 @@ public class drivetrainTest extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-        //Add Autonomous instructions here (functions) PS. Make sure to adjust.
+
         frontLeftMotor.setPower(1);
+        telemetry.addData("Step", "Front Left running");
+        telemetry.addData("Power", frontLeftMotor.getPower());
+        telemetry.update();
         sleep(1000);
-        frontRightMotor.setPower((1));
+
+        frontRightMotor.setPower(1);
+        telemetry.addData("Step", "Front Right running");
+        telemetry.addData("Power", frontRightMotor.getPower());
+        telemetry.update();
         sleep(1000);
-        backLeftMotor.setPower((1));
+
+        backLeftMotor.setPower(1);
+        telemetry.addData("Step", "Back Left running");
+        telemetry.addData("Power", backLeftMotor.getPower());
+        telemetry.update();
         sleep(1000);
-        backRightMotor.setPower((1));
+
+        backRightMotor.setPower(1);
+        telemetry.addData("Step", "Back Right running");
+        telemetry.addData("Power", backRightMotor.getPower());
+        telemetry.update();
         sleep(1000);
+
         frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
         backRightMotor.setPower(0);
+
+        telemetry.addData("Done", "All motors stopped");
+        telemetry.update();
     }
 }
