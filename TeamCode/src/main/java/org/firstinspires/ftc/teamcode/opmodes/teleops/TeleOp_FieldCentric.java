64db1527887f2aabbdefc5bd5975e5f216f6634b
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2025-11-21 22:09:11 -0700

    Major Issues fixed before Meet 1 - Redid sequence shooting logic - Switched out gamepad1 rotation and strafe joystick control - Intake is now controllable by the right trigger - Reversed intake direction to REVERSE - Undisabled TeleOp_FieldCentric.java - Added logic and updates code on TeleOp_FieldCentric based on TeleOp_1
    **TO DO**
    - Tune shooter velocity
    - Check sequence
    - Fix intake motor power and/or create velocity control for intake
    - Test TeleOp_FieldCentric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TeleOp_FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TeleOp_FieldCentric.java
@@ -20,7 +20,7 @@ public class TeleOp_FieldCentric extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontLeftMotor"); //CH Motor Port 0
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor"); //CH Motor Port 0
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor"); //CH Motor Port 1
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor"); //CH Motor Port 2
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor"); //CH Motor Port 3
@@ -47,14 +47,14 @@ public class TeleOp_FieldCentric extends LinearOpMode {
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
         shooter.setDirection(DcMotorSimple.Direction.FORWARD);
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -69,13 +69,13 @@ public class TeleOp_FieldCentric extends LinearOpMode {
 
 
             double y = -gamepad1.left_stick_y;
-            double rx = gamepad1.left_stick_x;
-            double x = gamepad1.right_stick_x;
+            double rx = gamepad1.right_stick_x;
+            double x = gamepad1.left_stick_x;
 
             double rotX = x * 0.9;
             double rotY = y;
 
-            boolean sequenceStarted = false;
+
 
             //use IMU heading
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
@@ -99,39 +99,44 @@ public class TeleOp_FieldCentric extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
-            /*
-            ORIGINAL SHOOTER SEQUENCE (unchanged)
+
             if (gamepad2.b && !sequenceStarted) {
                 shooter.setPower(1);
                 runtime.reset();
                 sequenceStarted = true;
             }
-            if (sequenceStarted && runtime.seconds() >= 1.5) {
-                pushythingy.setPosition(0);
-                sequenceStarted = false;
-            }
-            else pushythingy.setPosition(1);
-            */
 
-            if (gamepad2.right_trigger > 0.5) {
-                intake.setPower(-1);
-            } else {
-                intake.setPower(0);
+            if (sequenceStarted) {
+
+
+                if (runtime.seconds() >= 1.5) {
+                    intake.setPower(1);
+                }
+
+                if (runtime.seconds() >= 3.0) {
+                    pushythingy.setPosition(0);
+                }
             }
 
-            if (gamepad2.x) {
+
+            if (gamepad2.x) {        //"X" on Gamepad 2 stops both the intake and shooter
                 intake.setPower(0);
                 shooter.setPower(0);
             }
 
-            if (gamepad2.dpad_down) {
-                shooter.setVelocity(1650);
-                shooter.setPower(1);
+            if (gamepad2.b) {
+                shooter.setVelocity(1650); //tune
             }
 
             if (gamepad2.y) {
                 pushythingy.setPosition(0);
             } else pushythingy.setPosition(1);
+
+            if (gamepad2.right_trigger > 0.5) {
+                intake.setPower(1);
+            } else {
+                intake.setPower(0);
+            }
 
 
             if (gamepad2.options) {
