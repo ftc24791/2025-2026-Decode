2c63cd953c32feb01b4d7e7b29287f7e021f2de4
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2026-01-26 14:32:14 -0700

    Colour and Distance sensor test classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/mechanisms/tests/SensorDistanceTelemetry.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.opmodes.mechanisms.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+public class SensorDistanceTelemetry extends LinearOpMode {
+    private ColorSensor sensor_color;
+    private DistanceSensor sensor_distance;
+    double hue;
+
+    @Override
+    public void runOpMode() {
+
+        sensor_color = hardwareMap.get(ColorSensor.class, "sensor_color");
+        sensor_distance = hardwareMap.get(DistanceSensor.class,"sensor_color");
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Distance", sensor_distance.getDistance(DistanceUnit.CM));
+            telemetry.addData("Red", sensor_color.red());
+            telemetry.addData("Green", sensor_color.green());
+            telemetry.addData("Blue", sensor_color.blue());
+
+            if (sensor_distance.getDistance(DistanceUnit.CM) < 100) {
+                telemetry.addLine("it works");
+            }
+
+            telemetry.update();
+        }
+    }
+}
