8db459552c77711bb892c88087d306b486a25172
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2026-04-06 16:56:59 -0600

    Add name and group to DrivetrainControl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/DrivetrainControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/DrivetrainControl.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp (name = "Drivetrain Only", group = "A: Official Drive Control")
 public class DrivetrainControl extends LinearOpMode {
 
 
