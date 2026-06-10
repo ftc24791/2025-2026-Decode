984249ac21a29f2bc608c9dc3b03d4d1110ef144
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2026-06-10 16:44:27 -0600

    Make TankDrive independant of existing Hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/TankDrive.java
@@ -1,49 +1,38 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.opmodes.teleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
-import org.firstinspires.ftc.teamcode.opmodes.mechanisms.Hardware;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-//change display name when changes applied
-@TeleOp(name="Tank Drive - Put omni wheels front", group="A: Official Drive Control")
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+
+@TeleOp(name="Tank Drive", group="A: Official Drive Control")
 public class TankDrive extends LinearOpMode {
 
-    Hardware robot = new Hardware();
 
     @Override
     public void runOpMode() {
 
-        robot.init(hardwareMap);
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
+        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        frontLeftMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
+
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         double leftPower;
         double rightPower;
@@ -68,16 +57,16 @@ public class TankDrive extends LinearOpMode {
                 rightPower /= maximumPower;
             }
 
-            robot.frontLeftMotor.setPower(leftPower);
-            robot.backLeftMotor.setPower(leftPower);
-            robot.frontRightMotor.setPower(rightPower);
-            robot.backRightMotor.setPower(rightPower);
+            frontLeftMotor.setPower(leftPower);
+            backLeftMotor.setPower(leftPower);
+            frontRightMotor.setPower(rightPower);
+            backRightMotor.setPower(rightPower);
 
             //if this works implement in shooter and/or spindexer to test stall protection
-            telemetry.addData("FL Current, AMPS", robot.frontLeftMotor.getCurrent(CurrentUnit.AMPS));
-            telemetry.addData("FR Current, AMPS", robot.frontRightMotor.getCurrent(CurrentUnit.AMPS));
-            telemetry.addData("BL Current, AMPS", robot.backLeftMotor.getCurrent(CurrentUnit.AMPS));
-            telemetry.addData("BR Current, AMPS", robot.backRightMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("FL Current, AMPS", frontLeftMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("FR Current, AMPS", frontRightMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("BL Current, AMPS", backLeftMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("BR Current, AMPS", backRightMotor.getCurrent(CurrentUnit.AMPS));
 
         }
     }
