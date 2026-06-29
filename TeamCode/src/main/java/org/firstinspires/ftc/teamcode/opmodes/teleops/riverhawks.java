b2d611b46e87421f2f6df2fd2361e5db4a5ec868
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2026-06-29 15:13:28 -0600

    Add separate teleop for Riverhawks with simplified shooter logic - Remains independent to hardware class - Current measurement - Velocity telemetry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/riverhawks.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+
+@TeleOp(name="Riverhawks Baseball 26", group="A: Official Drive Control")
+public class riverhawks extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
+        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
+        DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        shooter.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+
+        frontLeftMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
+        shooter.setDirection(DcMotorEx.Direction.FORWARD); //change to reverse if shooter is spinning the wrong way
+
+        frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shooter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+        double leftPower;
+        double rightPower;
+        double maximumPower;
+
+
+        waitForStart();
+        if(isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+
+            leftPower = drive + turn;
+            rightPower = drive - turn;
+
+            maximumPower = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+
+            if (maximumPower > 1) {
+                leftPower /= maximumPower;
+                rightPower /= maximumPower;
+            }
+
+            if (gamepad1.aWasPressed()) {
+                shooter.setVelocity(9999); //maximum possible velocity hopefully
+            }
+            else if (gamepad1.aWasReleased()) {
+                shooter.setVelocity(0);
+            }
+
+            frontLeftMotor.setPower(leftPower);
+            backLeftMotor.setPower(leftPower);
+            frontRightMotor.setPower(rightPower);
+            backRightMotor.setPower(rightPower);
+
+            //if this works implement in shooter and/or spindexer to test stall protection
+            telemetry.addData("FL Current, AMPS", frontLeftMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("FR Current, AMPS", frontRightMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("BL Current, AMPS", backLeftMotor.getCurrent(CurrentUnit.AMPS));
+            telemetry.addData("BR Current, AMPS", backRightMotor.getCurrent(CurrentUnit.AMPS));
+
+            telemetry.addData("Shooter Velocity", shooter.getVelocity());
+            telemetry.update();
+
+        }
+    }
+}
