0e8eefec3945529674e26978201a725d0c12675f
Yusuf Kamal Ashraf <y.kamal26791@gmail.com>
2026-08-20 23:09:22 -0600

    Revert "test"
    This reverts commit 13ba4822e25b7e8168c7e02129fb67f9cd1d411c.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/vision/other/VisionFollow.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.opmodes.vision.other;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@Disabled
+@Autonomous
+public class VisionFollow extends LinearOpMode {
+
+    final double SPEED_TURN = 0.02; //Play with, chooses how fast motor moves
+    private DcMotor cameraMotor = null;
+    private static final boolean USE_WEBCAM = true;
+    private static final int DESIRED_TAG_ID = -1;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTagProcessor;
+    private AprilTagDetection desiredTag = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        boolean targetFound = false;    // Set to true when an AprilTag target is detected
+        double rotate = 0;        // Desired forward power/speed (-1 to +1)
+
+        //initAprilTag();
+
+        cameraMotor = hardwareMap.get(DcMotor.class, "cameraMotor");
+
+
+    }
+}
+
